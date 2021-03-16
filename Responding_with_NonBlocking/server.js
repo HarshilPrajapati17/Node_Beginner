303993737e844d084350cfdd10427853a797ff6f
HarshilPrajapati17 <harshil.prajapati@eronkan.com>
2021-03-16 14:26:31 +0530

    Responding with NonBlocking

---
+++ Responding_with_NonBlocking/server.js
@@ -0,0 +1,16 @@
+var http = require("http");
+var url = require('url');
+function start(route,handle) {
+    function onRequest(request, response) {
+        var pathname = url.parse(request.url).pathname;
+        console.log("Request received.");
+        route(handle, pathname, response);
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.end();
+    }
+    http.createServer(onRequest).listen(5000);
+    console.log("Server has started.");
+
+}
+
+exports.start = start;
